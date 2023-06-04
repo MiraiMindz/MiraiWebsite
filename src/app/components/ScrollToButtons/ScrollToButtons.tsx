4e7e99bfa845b424d7f39caf4088a8c854e963ad
MiraiMindz <78761663+MiraiMindz@users.noreply.github.com>
2023-06-04 19:35:35 -0300

    made some changes

--- src/app/components/ScrollToButtons/ScrollToButtons.tsx
+++ src/app/components/ScrollToButtons/ScrollToButtons.tsx
@@ -5,19 +5,32 @@ interface ScrollButtonProps {
 }
 
 export default function ScrollButton(props: ScrollButtonProps) {
-  let amount: number = props.value < 0 ? (window.innerHeight + Math.abs(props.value)) : (window.innerHeight - props.value)
-  const handleScroll = () => {
-    scroll.scrollMore(amount, {smooth:'easeInOutCubic'})
-  }
+  if (window !== undefined) {
+    let amount: number = props.value < 0 ? (window.innerHeight + Math.abs(props.value)) : (window.innerHeight - props.value)
+    const handleScroll = () => {
+      scroll.scrollMore(amount, {smooth:'easeInOutCubic'})
+    }
 
-  return (
-    <button className='scroll-down' onClick={handleScroll}>
-      <div>
-        <svg viewBox="0 0 142 142" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <rect x="71" y="141.711" width="100" height="20" rx="10" transform="rotate(-135 71 141.711)"/>
-          <rect x="71" y="141.711" width="20" height="100" rx="10" transform="rotate(-135 71 141.711)"/>
-        </svg>
-      </div>
-    </button>
-  );
+    return (
+      <button className='scroll-down' onClick={handleScroll}>
+        <div>
+          <svg viewBox="0 0 142 142" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect x="71" y="141.711" width="100" height="20" rx="10" transform="rotate(-135 71 141.711)"/>
+            <rect x="71" y="141.711" width="20" height="100" rx="10" transform="rotate(-135 71 141.711)"/>
+          </svg>
+        </div>
+      </button>
+    );
+  } else {
+    return (
+      <button className='scroll-down'>
+        <div>
+          <svg viewBox="0 0 142 142" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect x="71" y="141.711" width="100" height="20" rx="10" transform="rotate(-135 71 141.711)"/>
+            <rect x="71" y="141.711" width="20" height="100" rx="10" transform="rotate(-135 71 141.711)"/>
+          </svg>
+        </div>
+      </button>
+    );
+  }
 }
