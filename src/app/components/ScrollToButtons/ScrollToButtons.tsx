73e65636e4c2868fa78e956014fb7a4d41fbaf24
MiraiMindz <78761663+MiraiMindz@users.noreply.github.com>
2023-06-04 16:24:13 -0300

    made some changes

--- src/app/components/ScrollToButtons/ScrollToButtons.tsx
+++ src/app/components/ScrollToButtons/ScrollToButtons.tsx
@@ -1,3 +1,18 @@
+import { animateScroll as scroll } from 'react-scroll';
+
 export default function ScrollButton() {
-  return <h1>Scroll</h1>
+  const handleScroll = () => {
+    scroll.scrollMore((window.innerHeight + 64), {smooth:'easeInOutCubic'})
+  }
+
+  return (
+    <button className='scroll-down' onClick={handleScroll}>
+      <div>
+        <svg viewBox="0 0 142 142" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <rect x="71" y="141.711" width="100" height="20" rx="10" transform="rotate(-135 71 141.711)"/>
+          <rect x="71" y="141.711" width="20" height="100" rx="10" transform="rotate(-135 71 141.711)"/>
+        </svg>
+      </div>
+    </button>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 "use client";
-
 import Typewriter from "typewriter-effect";
+import ScrollButton from "./components/ScrollToButtons/ScrollToButtons";
 
 export default function Home() {
   return (
     <main className="min-h-screen min-w-full flex-grow">
-      <section className="min-h-screen flex flex-col flex-grow text-center justify-center items-center">
+      <section className="min-h-screen flex flex-col flex-grow text-center justify-center items-center relative">
         <Typewriter
           onInit={
             (typewriter) => {
@@ -18,11 +18,17 @@ export default function Home() {
             }
           }
         />
+        <div className="absolute bottom-32 md:bottom-8">
+          <ScrollButton />
+        </div>
       </section>
-      <section className="min-h-screen">
+      <section className="min-h-screen relative">
         <h1>Section 02</h1>
+        <div className="absolute bottom-32 md:bottom-8">
+          <ScrollButton />
+        </div>
       </section>
-      <section className="min-h-screen">
+      <section className="min-h-screen relative">
         <h1>Section 03</h1>
       </section>
     </main>
