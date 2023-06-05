f66720392a554dad3dd3ddaa46dd7c7c56f86ce7
MiraiMindz <78761663+MiraiMindz@users.noreply.github.com>
2023-06-05 11:57:03 -0300

    made some changes on mobile header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 "use client";
 import Typewriter from "typewriter-effect";
-import dynamic from 'next/dynamic';
-//import ScrollButton from "./components/ScrollToButtons/ScrollToButtons";
+import dynamic from "next/dynamic";
 import { useEffect, useState } from "react";
-import { animateScroll as scroll } from 'react-scroll';
+import { animateScroll as scroll } from "react-scroll";
 
-const ScrollButton = dynamic(() => import('./components/ScrollToButtons/ScrollToButtons'), {
-  ssr: false,
-})
+const ScrollButton = dynamic(
+  () => import("./components/ScrollToButtons/ScrollToButtons"),
+  {
+    ssr: false,
+  },
+);
 
 export default function Home() {
   const [scrollDown, setScrollDown] = useState<boolean>(false);
@@ -15,51 +17,83 @@ export default function Home() {
   const currentDate = new Date();
   const currentYear = currentDate.getFullYear();
   const handleScroll = () => {
-    scroll.scrollMore((window.innerHeight), {smooth:'easeInOutCubic'});
-  }
+    scroll.scrollMore(window.innerHeight, { smooth: "easeInOutCubic" });
+  };
   useEffect(() => {
     handleScroll();
   }, []);
-  
+
   return (
     <main className="min-h-screen min-w-full flex-grow">
       <section className="min-h-screen flex flex-col flex-grow text-center justify-center items-center relative text-lg md:text-2xl">
         <Typewriter
-          onInit={
-            (typewriter) => {
-              typewriter
-                .typeString("Onde a criatividade e o tecnicismo dão um passo em direção ao <span class='underline'>futuro</span>.")
-                .pauseFor(500)
-                .callFunction(() => {
-                  handleScrollDown();
-                })
+          onInit={(typewriter) => {
+            typewriter
+              .typeString(
+                "Onde a criatividade e o tecnicismo dão um passo em direção ao <span class='underline'>futuro</span>.",
+              )
+              .pauseFor(500)
+              .callFunction(() => {
+                handleScrollDown();
+              })
               .start();
-            }
-          }
+          }}
         />
-        <div className={(scrollDown ? "toggleScrollDownOn" : "toggleScrollDownOff") + " absolute bottom-8 md:bottom-8"}>
-          <ScrollButton value={48}/>
+        <div
+          className={(scrollDown
+            ? "toggleScrollDownOn"
+            : "toggleScrollDownOff") + " absolute bottom-8 md:bottom-8"}
+        >
+          <ScrollButton value={48} />
         </div>
       </section>
-      <section className={(scrollDown ? "block" : "hidden") + " min-h-screen relative flex flex-col flex-grow justify-center items-center"}>
-        <h1 className="font-black block text-left absolute top-2 w-full text-2xl">Quem sou eu?</h1>
+      <section
+        className={(scrollDown ? "block" : "hidden") +
+          " min-h-screen relative flex flex-col flex-grow justify-center items-center"}
+      >
+        <h1 className="font-black block text-left absolute top-2 w-full text-2xl">
+          Quem sou eu?
+        </h1>
         <p className="md:mx-16 md:text-2xl">
-          Meu nome é Bruno Barreto, mas as pessoas me conhecem como Mirai. Eu tenho {(currentYear - 2005)} anos, e sou principalmente um desenvolvedor Full-Stack; embora também faça produção musical, design e escrita como hobbies. 
+          Meu nome é Bruno Barreto, mas as pessoas me conhecem como Mirai. Eu
+          tenho {currentYear - 2005}{" "}
+          anos, e sou principalmente um desenvolvedor Full-Stack; embora também
+          faça produção musical, design e escrita como hobbies.
         </p>
-        <div className={(scrollDown ? "toggleScrollDownOn" : "toggleScrollDownOff") + " absolute bottom-32 md:bottom-16 md:block hidden"}>
-          <ScrollButton value={16}/>
+        <div
+          className={(scrollDown
+            ? "toggleScrollDownOn"
+            : "toggleScrollDownOff") +
+            " absolute bottom-32 md:bottom-16 md:block hidden"}
+        >
+          <ScrollButton value={16} />
         </div>
 
-        <div className={(scrollDown ? "toggleScrollDownOn" : "toggleScrollDownOff") + " absolute bottom-32 md:bottom-8 block md:hidden"}>
-          <ScrollButton value={-64}/>
+        <div
+          className={(scrollDown
+            ? "toggleScrollDownOn"
+            : "toggleScrollDownOff") +
+            " absolute bottom-32 md:bottom-8 block md:hidden"}
+        >
+          <ScrollButton value={-64} />
         </div>
       </section>
-      <section className={(scrollDown ? "block" : "hidden") + " min-h-screen relative flex flex-col flex-grow justify-center items-center"}>
-        <h1 className="font-black block text-left absolute top-2 w-full text-2xl">Recomendações</h1>
+      <section
+        className={(scrollDown ? "block" : "hidden") +
+          " min-h-screen relative flex flex-col flex-grow justify-center items-center"}
+      >
+        <h1 className="font-black block text-left absolute top-2 w-full text-2xl">
+          Recomendações
+        </h1>
         <p className="text-center md:text-2xl">
-          Porquê você não dá uma olhada nas outras seções do site? <br/><br/>
-          <span className="hidden md:inline-block text-neutral-500 dark:text-neutral-400">Basta usar o menu superior</span>
-          <span className="inline-block md:hidden text-neutral-500 dark:text-neutral-400">basta usar o menu no canto superior-direito da tela</span>
+          Porquê você não dá uma olhada nas outras seções do site? <br />
+          <br />
+          <span className="hidden md:inline-block text-neutral-500 dark:text-neutral-400">
+            Basta usar o menu superior
+          </span>
+          <span className="inline-block md:hidden text-neutral-500 dark:text-neutral-400">
+            basta usar o menu no canto superior-direito da tela
+          </span>
         </p>
       </section>
     </main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {},
+  theme: {
+    screens: {
+      'xs': '475px',
+      ...defaultTheme.screens,
+    },
+  },
   darkMode: 'class',
   plugins: [
     require('tailwind-scrollbar'),
