fe607d72692c925b6d976d8385fdb07ea85e8e68
MiraiMindz <78761663+MiraiMindz@users.noreply.github.com>
2023-06-07 13:17:17 -0300

    made some changes

--- src/app/lib/mdx/parsePosts.ts
+++ src/app/lib/mdx/parsePosts.ts
@@ -1,27 +1,42 @@
 import fs from 'fs';
 import path from 'path';
 import { compileMDX } from 'next-mdx-remote/rsc';
+import { ReactElement } from 'react';
 
-  const rootDirectory = path.join(process.cwd(), 'src', 'app', 'blog', 'content', 'posts');
+const rootDirectory = path.join(process.cwd(), 'src', 'app', 'blog', 'content', 'posts');
 
-export const getPostBySlug = async (slug: any) => {
+type Frontmatter = {
+  title: string;
+  // Add other frontmatter properties as needed
+};
+
+type Post = {
+  meta: {
+    slug: string;
+    title: string;
+    // Add other meta properties as needed
+  };
+  content: ReactElement;
+};
+
+export const getPostBySlug = async (slug: string): Promise<Post> => {
   const realSlug = slug.replace(/\.mdx$/, '');
   const filePath = path.join(rootDirectory, `${realSlug}.mdx`);
 
   const fileContent = fs.readFileSync(filePath, { encoding: 'utf8' });
 
-  const { frontmatter, content } = await compileMDX({
+  const { frontmatter, content } = await compileMDX<Frontmatter>({
     source: fileContent,
     options: { parseFrontmatter: true }
   });
 
-  return { meta: { ...frontmatter, slug: realSlug }, content }
-}
+  return { meta: { ...frontmatter, slug: realSlug }, content };
+};
 
-export const getAllPostsMeta = async () => {
-  const files = fs.readdirSync(rootDirectory)
+export const getAllPostsMeta = async (): Promise<Frontmatter[]> => {
+  const files = fs.readdirSync(rootDirectory);
 
-  let posts = []
+  let posts: Frontmatter[] = [];
 
   for (const file of files) {
     const { meta } = await getPostBySlug(file);
@@ -29,4 +44,5 @@ export const getAllPostsMeta = async () => {
   }
 
   return posts;
-}
+};
+
