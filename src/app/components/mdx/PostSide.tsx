81747eab4e1b820d38ecb1157136153a2b0d252e
MiraiMindz <78761663+MiraiMindz@users.noreply.github.com>
2023-06-11 20:35:10 -0300

    made some changes

--- src/app/components/mdx/PostSide.tsx
+++ src/app/components/mdx/PostSide.tsx
@@ -10,8 +10,8 @@ interface PostSideProps {
 
 export function PostSide(props: PostSideProps) {
   return (
-    <Link href={props.href} className="p-5 border-2 rounded-lg border-neutral-950 dark:border-neutral-50 w-60 truncate">
-      <div>
+    <Link href={props.href}>
+      <div className="p-5 border-2 rounded-lg border-neutral-950 dark:border-neutral-50 w-60 truncate">
         <h2 className="font-black">{props.title}</h2>
         <p className="italic text-neutral-500">{props.shortSum}</p>
         <p className="text-neutral-500">{props.date}</p>
