c35af5138ed483ff8af5b71784dc0ad7175ee8c3
MiraiMindz <78761663+MiraiMindz@users.noreply.github.com>
2023-06-11 20:40:10 -0300

    made some changes

--- src/app/components/mdx/PostSide.tsx
+++ src/app/components/mdx/PostSide.tsx
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import dayjs from 'dayjs';
+import 'dayjs/locale/pt-br';
 
 interface PostSideProps {
   title: string;
@@ -9,13 +11,14 @@ interface PostSideProps {
 }
 
 export function PostSide(props: PostSideProps) {
+  const formattedDate = dayjs(props.date).locale('pt-br').format('D/MM/YYYY');
   return (
     <Link href={props.href}>
-      <div className="p-5 border-2 rounded-lg border-neutral-950 dark:border-neutral-50 w-60 truncate">
-        <h2 className="font-black">{props.title}</h2>
-        <p className="italic text-neutral-500">{props.shortSum}</p>
-        <p className="text-neutral-500">{props.date}</p>
-        <p className="text-neutral-500 underline">{props.readTime}</p>
+      <div className="p-5 border-2 rounded-lg border-neutral-950 dark:border-neutral-50 truncate mr-0 hover:mr-4">
+        <h2 className="font-black font-base">{props.title}</h2>
+        <p className="italic text-neutral-500 font-sm">{props.shortSum}</p>
+        <p className="text-neutral-500 font-sm">{formattedDate}</p>
+        <p className="text-neutral-500 font-sm underline">{props.readTime}</p>
       </div>
     </Link>
   );
