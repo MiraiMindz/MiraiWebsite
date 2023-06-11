ebdd66179e524afb161ca8967df3cdeb3cbf6e84
MiraiMindz <78761663+MiraiMindz@users.noreply.github.com>
2023-06-11 20:29:19 -0300

    made some changes

--- src/app/components/mdx/PostSide.tsx
+++ src/app/components/mdx/PostSide.tsx
@@ -1,11 +1,22 @@
-interface PostSideProps {
+import Link from "next/link";
 
+interface PostSideProps {
+  title: string;
+  shortSum: string;
+  href: string;
+  readTime: string;
+  date: string;
 }
 
 export function PostSide(props: PostSideProps) {
   return (
-    <div>
-
-    </div>
+    <Link href={props.href} className="p-5 border-2 rounded-lg border-neutral-950 dark:border-neutral-50 w-60 truncate">
+      <div>
+        <h2 className="font-black">{props.title}</h2>
+        <p className="italic text-neutral-500">{props.shortSum}</p>
+        <p className="text-neutral-500">{props.date}</p>
+        <p className="text-neutral-500 underline">{props.readTime}</p>
+      </div>
+    </Link>
   );
 }
