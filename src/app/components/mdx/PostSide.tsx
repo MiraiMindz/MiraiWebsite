d7f339495455bf8881f1b892cb29b26acf4c8695
MiraiMindz <78761663+MiraiMindz@users.noreply.github.com>
2023-06-12 11:26:01 -0300

    made some changes

--- src/app/components/mdx/PostSide.tsx
+++ src/app/components/mdx/PostSide.tsx
@@ -15,7 +15,7 @@ export function PostSide(props: PostSideProps) {
   return (
     <Link href={props.href}>
       <div className="postSideClass">
-        <h2 className="font-black text-base truncate transition-all text-left md:text-center">{props.title}</h2>
+        <h2 className="font-black text-base truncate transition-all text-left md:whitespace-normal">{props.title}</h2>
         <p className="italic text-neutral-500 text-sm text-wrap md:truncate">{props.shortSum}</p>
         <p className="text-neutral-500 text-sm truncate">Publicado em: {formattedDate}</p>
         <p className="text-neutral-500 text-sm underline truncate">{props.readTime}</p>
