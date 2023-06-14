b159760bb358187f46ead36bed7682710bb0e4d0
MiraiMindz <78761663+MiraiMindz@users.noreply.github.com>
2023-06-14 12:10:25 -0300

    fixed the header text issue

--- src/app/components/mdx/headers.tsx
+++ src/app/components/mdx/headers.tsx
@@ -1,41 +1,53 @@
-export function H1({children}:any) {
-  const idText = children?.replace(/ /g, "-").toLowerCase();
+import { ReactElement } from "react";
+
+interface HeadersProps {
+  children: string;
+}
+
+export function H1(props: HeadersProps) {
+  let pstr = String(props.children);
+  const idText = typeof pstr === 'string' ? pstr.replace(/ /g, "-").toLowerCase() : props.children.toString().replace(/ /g, "-").toLowerCase();
   return (
-    <h1 id={idText} className="underline text-3xl my-2">{children}</h1>
+    <h1 id={idText} className="underline text-3xl my-2">{props.children}</h1>
   )
 }
 
-export function H2({children}:any) {
-  const idText = children?.replace(/ /g, "-").toLowerCase();
+export function H2(props: HeadersProps) {
+  let pstr = String(props.children);
+  const idText = typeof pstr === 'string' ? pstr.replace(/ /g, "-").toLowerCase() : props.children.toString().replace(/ /g, "-").toLowerCase();
   return (
-  <h2 id={idText} className="underline text-2xl my-2">{children}</h2>
+    <h2 id={idText} className="underline text-2xl my-2">{props.children}</h2>
   );
 }
 
-export function H3({children}:any) {
-  const idText = children?.replace(/ /g, "-").toLowerCase();
+export function H3(props: HeadersProps) {
+  let pstr = String(props.children);
+  const idText = typeof pstr === 'string' ? pstr.replace(/ /g, "-").toLowerCase() : props.children.toString().replace(/ /g, "-").toLowerCase();
   return (
-  <h3 id={idText} className="underline text-xl my-2">{children}</h3>
+    <h3 id={idText} className="underline text-xl my-2">{props.children}</h3>
   );
 }
 
-export function H4({children}:any) {
-  const idText = children?.replace(/ /g, "-").toLowerCase();
+export function H4(props: HeadersProps) {
+  let pstr = String(props.children);
+  const idText = typeof pstr === 'string' ? pstr.replace(/ /g, "-").toLowerCase() : props.children.toString().replace(/ /g, "-").toLowerCase();
   return (
-  <h4 id={idText} className="underline text-lg my-2">{children}</h4>
+    <h4 id={idText} className="underline text-lg my-2">{props.children}</h4>
   );
 }
 
-export function H5({children}:any) {
-  const idText = children?.replace(/ /g, "-").toLowerCase();
+export function H5(props: HeadersProps) {
+  let pstr = String(props.children);
+  const idText = typeof pstr === 'string' ? pstr.replace(/ /g, "-").toLowerCase() : props.children.toString().replace(/ /g, "-").toLowerCase();
   return (
-  <h5 id={idText} className="underline text-base my-2">{children}</h5>
+    <h5 id={idText} className="underline text-base my-2">{props.children}</h5>
   );
 }
 
-export function H6({children}:any) {
-  const idText = children?.replace(/ /g, "-").toLowerCase();
+export function H6(props: HeadersProps) {
+  let pstr = String(props.children);
+  const idText = typeof pstr === 'string' ? pstr.replace(/ /g, "-").toLowerCase() : props.children.toString().replace(/ /g, "-").toLowerCase();
   return (
-  <h6 id={idText} className="underline text-sm my-2">{children}</h6>
+    <h6 id={idText} className="underline text-sm my-2">{props.children}</h6>
   );
 }
