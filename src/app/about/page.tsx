d21d2ad4e8e1999f959020999afbc9e622e5e551
Mirai <78761663+MiraiMindz@users.noreply.github.com>
2023-06-16 19:15:18 -0300

    made some changes

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,6 +3,8 @@ import Image from 'next/image';
 
 export default function About() {
   const imgSize: number = 128;
+  const currentYear: number = new Date().getFullYear();
+  const endingYear: number = 2026;
   return (
     <main className="min-h-screen min-w-full flex-grow flex flex-row justify-center items-start">
       <article className="w-[40ch] md:w-[80ch]">
@@ -17,24 +19,44 @@ export default function About() {
               alt="Foto do autor"
             />
             <p>
-              (apresentação)
+            Meu nome é Bruno Barreto, mas as pessoas me conhecem como Mirai. Eu
+            tenho {currentYear - 2005}{" "}
+            anos, e sou principalmente um desenvolvedor Full-Stack; embora também
+            faça produção musical, design e escrita como hobbies.
+            </p>
+          </div>
+        </section>
+        <section className="my-2 md:my-4">
+          <h1 className="font-black text-xl">Porquê você se chama Mirai?</h1>
+          <div className="flex flex-row justify-between items-center">
+            <p>
+              Mirai é o alter-ego criado por mim aos 16 anos para retornar com produção musical, inspirado no fato de amigos dizerem que "sou o futuro", foi durante a produção de uma música com temática japonêsa e significa "Futuro".
             </p>
           </div>
         </section>
         <section className="my-2 md:my-4">
           <h1 className="font-black text-xl">Formações</h1>
           <div className="flex flex-row justify-between items-center">
-            <p>
-              (Formações Academicas)
-            </p>
-          </div>
-        </section>
-        <section className="my-2 md:my-4">
-          <h1 className="font-black text-xl">Formas de Contato</h1>
-          <div className="flex flex-row justify-between items-center">
-            <p>
-              (Formas de Contato)
-            </p>
+            <div>
+              <p>UERJ -Tec. Análise e Desenvolvimento de Sistemas</p>
+              <p>{`(2023 - ${currentYear == endingYear ? "Atualidade" : endingYear.toString()})`}</p>
+            </div>
+            <div>
+              <p>Colégio Estadual Círculo Operário</p>
+              <p>(2021 - 2022)</p>
+            </div>
+            <div>
+              <p>Centro de Formação Nogueira Mineiro</p>
+              <p>(2020)</p>
+            </div>
+            <div>
+              <p>Instituto PROA</p>
+              <p>(08/2022  - 11/2022)</p>
+            </div>
+            <div>
+              <p>RocketSeat Discover</p>
+              <p>09/2022</p>
+            </div>
           </div>
         </section>
       </article>
