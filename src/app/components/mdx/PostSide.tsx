bf3da653c2de1c26ae29bb166a1c6685f24629fe
Mirai <78761663+MiraiMindz@users.noreply.github.com>
2023-06-16 17:01:49 -0300

    made some changes

--- src/app/components/mdx/PostSide.tsx
+++ src/app/components/mdx/PostSide.tsx
@@ -52,7 +52,7 @@ export function ClassCard(props: ClassCardProps) {
   return (
     <Link href={props.href} className="w-[40ch] md:w-[80ch]">
       <div className="postCardClass w-fit">
-        <h2 className="font-black transition-all text-left text-lg">{`${props.chapter} - ${props.title}`}</h2>
+        <h2 className="font-black truncate transition-all text-left text-lg">{`${props.chapter} - ${props.title}`}</h2>
         <p className="italic text-neutral-500 dark:text-neutral-400 whitespace-normal text-base">{props.shortSum}</p>
         <p className="text-neutral-500 dark:text-neutral-400 underline text-base">{props.readTime}</p>
       </div>
