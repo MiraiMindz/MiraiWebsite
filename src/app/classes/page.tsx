829ee5d7cc60814bd5e77f978422f854a799a671
Mirai <78761663+MiraiMindz@users.noreply.github.com>
2023-06-16 20:58:22 -0300

    made some changes

--- src/app/classes/page.tsx
+++ src/app/classes/page.tsx
@@ -2,10 +2,14 @@
 
 import Link from "next/link";
 
+import PythonLogo from "../components/assets/PythonLogo";
 
 export default function Classes() {
   return (
     <main>
+      <div className="w-8 h-8">
+      <PythonLogo/>
+      </div>
       <Link href="/classes/PythonLessons">PythonLessons</Link>
     </main>
   );
