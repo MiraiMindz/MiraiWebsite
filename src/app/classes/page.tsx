43bc49f450c0864ad06e0698d959596e5f97fb98
Mirai <78761663+MiraiMindz@users.noreply.github.com>
2023-06-17 17:27:38 -0300

    added python logo

--- src/app/classes/page.tsx
+++ src/app/classes/page.tsx
@@ -1,10 +1,15 @@
 "use client";
+import PythonLogo from "../components/assets/PythonLogo";
 
 import Link from "next/link";
 
 export default function Classes() {
   return (
     <main>
+      <PythonLogo
+        blueClass="fill-blue-700 dark:fill-blue-400"
+        yellowClass="text-yellow-600 dark:text-yellow-400"
+      />
       <Link href="/classes/PythonLessons">PythonLessons</Link>
     </main>
   );
