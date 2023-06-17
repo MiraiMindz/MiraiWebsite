f44449cd6f157b711973f858211e01aa9d587cfb
Mirai <78761663+MiraiMindz@users.noreply.github.com>
2023-06-17 17:37:13 -0300

    made some changes

--- src/app/classes/page.tsx
+++ src/app/classes/page.tsx
@@ -6,11 +6,24 @@ import Link from "next/link";
 export default function Classes() {
   return (
     <main>
-      <PythonLogo
-        blueClass="fill-blue-700 dark:fill-blue-400"
-        yellowClass="fill-yellow-600 dark:fill-yellow-400"
-      />
-      <Link href="/classes/PythonLessons">PythonLessons</Link>
+      <Link href="/classes/PythonLessons">
+        <div className="flex flex-col justify-between items-center p-2 m-2 border-2 rounded-md border-neutral-950 dark:border-neutral-50">
+          <div className="flex justify-between items-center">
+            <div className="rounded-full border-neutral-950 dark:border-neutral-50 border-2 p-2">
+              <PythonLogo
+                blueClass="fill-blue-400"
+                yellowClass="fill-yellow-400"
+              />
+            </div>
+            <div className="block text-center">
+              <h1>Python Lessons</h1>
+            </div>
+          </div>
+          <p className="mt-2 border-neutral-950/60 dark:border-neutral-50/60">
+            Um curso de Python escrito por mim para armazenar meu conhecimento na linguagem e ajudar as pessoas que desejam entrar no mundo de desenvolvimento ou apenas aprender Python.
+          </p>
+        </div>
+      </Link>
     </main>
   );
 }
