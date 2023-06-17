9061c2fd41011542710b5dd5cd67a236fc0d4eb4
Mirai <78761663+MiraiMindz@users.noreply.github.com>
2023-06-17 16:54:10 -0300

    made some changes

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,6 @@
 "use client";
-import Image from 'next/image';
 import Typewriter from "typewriter-effect";
+import Image from 'next/image';
 
 export default function About() {
   const imgSize: number = 160;
