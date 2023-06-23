14004b5e985c5960e4909f81d766bda8bc160132
Mirai <78761663+MiraiMindz@users.noreply.github.com>
2023-06-23 18:25:28 -0300

    made some changes

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -20,6 +20,7 @@ export default async function Projects() {
         console.error('Error fetching data:', error);
       });
   }, []);
+  
   useEffect(() => {
     console.log("repoData", repoData);
   }, [repoData]);
