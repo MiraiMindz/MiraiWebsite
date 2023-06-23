aeec7dc3413f4612cfb4d9655058a0df50b86107
MiraiMindz <78761663+MiraiMindz@users.noreply.github.com>
2023-06-23 11:41:25 -0300

    made some changes

--- src/app/lib/APIs/GitHub/Projects.ts
+++ src/app/lib/APIs/GitHub/Projects.ts
@@ -1,42 +1,47 @@
 import axios from 'axios';
-export async function fetchGithubPublicRepos(token: string) {
-  try {
-    const response = await axios.get('https://api.github.com/user/repos', {
-      headers: {
-          Authorization: `Bearer ${token}`,
-      },
-    });
+import 'dotenv/config';
 
-    const repositories = await Promise.all(
-    response.data.map(async (repo: any) => {
-      const readmeResponse = await axios.get(
-          `https://api.github.com/repos/${repo.full_name}/readme`, {
-            headers: {
+export async function fetchGithubPublicRepos() {
+  const token: string | undefined = process.env.API_KEY;
+  if (token != undefined) {
+    try {
+      const response = await axios.get('https://api.github.com/user/repos', {
+        headers: {
             Authorization: `Bearer ${token}`,
-            Accept: 'application/vnd.github.VERSION.raw',
-            },
-          }
-        );
+        },
+      });
 
-        return {
-          id: repo.id,
-          name: repo.name,
-          description: repo.description,
-          html_url: repo.html_url,
-          readme: readmeResponse.data,
-        };
-      })
-    );
+      const repositories = await Promise.all(
+      response.data.map(async (repo: any) => {
+        const readmeResponse = await axios.get(
+            `https://api.github.com/repos/${repo.full_name}/readme`, {
+              headers: {
+              Authorization: `Bearer ${token}`,
+              Accept: 'application/vnd.github.VERSION.raw',
+              },
+            }
+          );
 
-    return {
-      props: {
-        repositories,
-      },
-    };
-  } catch (error) {
-    console.error('Error fetching repositories:', error);
-    return {
-      repositories: []
-    };
+          return {
+            id: repo.id,
+            name: repo.name,
+            description: repo.description,
+            html_url: repo.html_url,
+            readme: readmeResponse.data,
+          };
+        })
+      );
+
+      return {
+        props: {
+          repositories,
+        },
+      };
+    } catch (error) {
+      console.error('Error fetching repositories:', error);
+      return {
+        repositories: []
+      };
+    }
   }
 }
