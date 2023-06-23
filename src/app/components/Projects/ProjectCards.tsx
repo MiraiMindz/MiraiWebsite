ee1324254c866b8f05fa5129b3b8725e7fce1a4f
Mirai <78761663+MiraiMindz@users.noreply.github.com>
2023-06-23 19:21:42 -0300

    made some changes

--- src/app/components/Projects/ProjectCards.tsx
+++ src/app/components/Projects/ProjectCards.tsx
@@ -9,10 +9,10 @@ interface ProjectCardProps {
 export function ProjectCard(props: ProjectCardProps) {
   return (
     <Link href={props.htmlURL}>
-      <div className="rounded-lg border-2 border-neutral-950 dark:border-neutral-50 p-2 grid grid-rows-3 grid-flow-col gap-y-4">
-        <h1 className="text-balance font-ibmPlexMono font-black text-xl text-center">{props.repoName}</h1>
-        <p className="text-center">{props.repoDescription}</p>
-        <div className="grid grid-rows-2 grid-flow-col gap-y-2">
+      <div className="rounded-lg border-2 border-neutral-950 dark:border-neutral-50 p-2 flex flex-col justify-start items-center">
+        <h1 className="text-balance font-ibmPlexMono font-black text-xl text-center my-2">{props.repoName}</h1>
+        <p className="text-center my-2">{props.repoDescription}</p>
+        <div className="grid grid-rows-2 grid-flow-col gap-y-2 my-2">
           <p className="text-sm opacity-60 text-center">Para vê-lo no GitHub clique no cartão ou no link abaixo.</p>
           <a href={props.htmlURL} target="_blank" className="text-sm transition-all text-blue-400 hover:text-blue-600 hover:text-violet-400 dark:hover:text-violet-600 text-center">Veja no GitHub.</a>
         </div>
