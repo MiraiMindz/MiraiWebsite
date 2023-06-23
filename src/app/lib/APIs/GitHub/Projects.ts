d894e121abc72f19dc5f8ee1b708e1a6802751d9
MiraiMindz <78761663+MiraiMindz@users.noreply.github.com>
2023-06-23 12:09:00 -0300

    made some changes

--- src/app/lib/APIs/GitHub/Projects.ts
+++ src/app/lib/APIs/GitHub/Projects.ts
@@ -14,44 +14,5 @@ export async function fetchGithubPublicRepos() {
       });
 
       console.log("repoData", repoData);
-
-
-    // try {
-    //   const response = await axios.get('https://api.github.com/user/repos', {
-    //     headers: {
-    //         Authorization: `Bearer ${token}`,
-    //     },
-    //   });
-
-    //   console.log(response.data);
-
-    //   const repositories = await Promise.all(
-    //     response.data.map(async (repo: any) => {
-    //       const readmeResponse = await axios.get(
-    //         `https://api.github.com/repos/${repo.full_name}/readme`, {
-    //           headers: {
-    //           Authorization: `Bearer ${token}`,
-    //           Accept: 'application/vnd.github+json',
-    //           },
-    //         }
-    //       );
-
-    //       console.log(readmeResponse);
-
-    //       return {
-    //         id: repo.id,
-    //         name: repo.name,
-    //         description: repo.description,
-    //         html_url: repo.html_url,
-    //         readme: readmeResponse.data,
-    //       };
-    //     })
-    //   );
-
-    //   return repositories;
-    // } catch (error) {
-    //   console.error('Error fetching repositories:', error);
-    //   return [];
-    // }
   }
 }
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -4,13 +4,27 @@ import { useEffect } from "react";
 import { fetchGithubPublicRepos } from "../lib/APIs/GitHub/Projects";
 
 export default async function Projects() {
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const GitRepos = await fetchGithubPublicRepos();
+  //     console.log(GitRepos);
+  //   }
+  //   fetchData();
+  // }, []);
+
   useEffect(() => {
-    const fetchData = async () => {
-      const GitRepos = await fetchGithubPublicRepos();
-      console.log(GitRepos);
-    }
-    fetchData();
+    let repoData;
+    const apiUrl = 'https://api.github.com/users/miraimindz/repos';
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('This is your data', data);
+        repoData = data;
+      });
+
+      console.log("repoData", repoData);
   }, []);
+
   return (
     <main>
       Projects
