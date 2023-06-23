d2836ff646dee6e9c6fae4dda4167f46946b0783
MiraiMindz <78761663+MiraiMindz@users.noreply.github.com>
2023-06-23 11:27:23 -0300

    made some changes

--- src/app/lib/APIs/GitHub/Projects.ts
+++ src/app/lib/APIs/GitHub/Projects.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 export async function fetchGithubPublicRepos() {
   try {
-    const token: string = String(process.env.GITHUB_API_KEY);
+    const token: string = String(process.env.API_KEY);
     console.log(token);
     const response = await axios.get('https://api.github.com/user/repos', {
       headers: {
