869079180e8aaa95999f78abdebf42ab268d0d9b
MiraiMindz <78761663+MiraiMindz@users.noreply.github.com>
2023-06-23 12:06:09 -0300

    made some changes

--- src/app/lib/APIs/GitHub/Projects.ts
+++ src/app/lib/APIs/GitHub/Projects.ts
@@ -1,46 +1,57 @@
-import axios from 'axios';
 import 'dotenv/config';
 
 export async function fetchGithubPublicRepos() {
-  const token: string | undefined = process.env.API_KEY?.toString();
+  const token: string | undefined = process.env.API_KEY;
   if (token != undefined) {
-    console.log(token.toString())
-    try {
-      const response = await axios.get('https://api.github.com/user/repos', {
-        headers: {
-            Authorization: `Bearer ${token}`,
-        },
+    console.log(token);
+    let repoData;
+    const apiUrl = 'https://api.github.com/users/miraimindz/repos';
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('This is your data', data);
+        repoData = data;
       });
 
-      console.log(response.data);
+      console.log("repoData", repoData);
 
-      const repositories = await Promise.all(
-        response.data.map(async (repo: any) => {
-          const readmeResponse = await axios.get(
-            `https://api.github.com/repos/${repo.full_name}/readme`, {
-              headers: {
-              Authorization: `Bearer ${token}`,
-              Accept: 'application/vnd.github+json',
-              },
-            }
-          );
 
-          console.log(readmeResponse);
+    // try {
+    //   const response = await axios.get('https://api.github.com/user/repos', {
+    //     headers: {
+    //         Authorization: `Bearer ${token}`,
+    //     },
+    //   });
 
-          return {
-            id: repo.id,
-            name: repo.name,
-            description: repo.description,
-            html_url: repo.html_url,
-            readme: readmeResponse.data,
-          };
-        })
-      );
+    //   console.log(response.data);
 
-      return repositories;
-    } catch (error) {
-      console.error('Error fetching repositories:', error);
-      return [];
-    }
+    //   const repositories = await Promise.all(
+    //     response.data.map(async (repo: any) => {
+    //       const readmeResponse = await axios.get(
+    //         `https://api.github.com/repos/${repo.full_name}/readme`, {
+    //           headers: {
+    //           Authorization: `Bearer ${token}`,
+    //           Accept: 'application/vnd.github+json',
+    //           },
+    //         }
+    //       );
+
+    //       console.log(readmeResponse);
+
+    //       return {
+    //         id: repo.id,
+    //         name: repo.name,
+    //         description: repo.description,
+    //         html_url: repo.html_url,
+    //         readme: readmeResponse.data,
+    //       };
+    //     })
+    //   );
+
+    //   return repositories;
+    // } catch (error) {
+    //   console.error('Error fetching repositories:', error);
+    //   return [];
+    // }
   }
 }
