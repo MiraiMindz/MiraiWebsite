b1dbd9c4701bd44e1ae0571c4ea1a173841460a1
Mirai <78761663+MiraiMindz@users.noreply.github.com>
2023-06-23 19:47:48 -0300

    made some changes

--- src/app/components/Projects/ProjectCards.tsx
+++ src/app/components/Projects/ProjectCards.tsx
@@ -8,7 +8,7 @@ interface ProjectCardProps {
 
 export function ProjectCard(props: ProjectCardProps) {
   return (
-    <div className="rounded-lg border-2 border-neutral-950 dark:border-neutral-50 p-2 flex flex-col justify-start items-center ProjectCard transition-all">
+    <div className="rounded-lg border-2 border-neutral-950 dark:border-neutral-50 p-2 flex flex-col justify-start items-center ProjectCard transition-all hover:border-violet-500 hover:dark:border-violet-400">
       <h1 className="text-balance font-ibmPlexMono font-black text-xl text-center my-2 transition-all">{props.repoName}</h1>
       <p className="text-justify my-2 transition-all">{props.repoDescription}</p>
       <div className="grid grid-rows-2 grid-flow-col gap-y-2 my-2">
