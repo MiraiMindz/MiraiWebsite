7aedad2034f0af3ad8c68b3041f18c4762ecbe6f
Mirai <78761663+MiraiMindz@users.noreply.github.com>
2023-06-23 18:46:31 -0300

    made some changes

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -5,23 +5,36 @@ import { useEffect, useState } from "react";
 import { GitHubRepoType } from "../types/GitHubRepos";
 import { ProjectCard } from "../components/Projects/ProjectCards";
 
+async function getData() {
+  const apiUrl = 'https://api.github.com/users/miraimindz/repos';
+  const res = await fetch(apiUrl)
+      // .then((response) => response.json())
+      // .then((data: any) => {
+      //   console.log("data", data);
+      //   setRepoData(...data);
+      //   setIsLoading(false);
+      // })
+      // .catch((error) => {
+      //   console.error('Error fetching data:', error);
+      //   setIsLoading(false);
+      // });
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+ 
+  return res.json()
+}
+
 export default async function Projects() {
   const [repoData, setRepoData] = useState<any[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
-    const apiUrl = 'https://api.github.com/users/miraimindz/repos';
-    fetch(apiUrl)
-      .then((response) => response.json())
-      .then((data: any) => {
-        console.log("data", data);
-        setRepoData(data);
-        setIsLoading(false);
-      })
-      .catch((error) => {
-        console.error('Error fetching data:', error);
-        setIsLoading(false);
-      });
+    const fetchData = async () => {
+      let tempData = await getData();
+      setRepoData(tempData);
+    }
+    fetchData();
   }, []);
   
   useEffect(() => {
@@ -30,26 +43,21 @@ export default async function Projects() {
     }
   }, [repoData]);
 
-  while (isLoading) {
-    if (isLoading) {
-      return <div>Loading...</div>; // Render a loading state or spinner while fetching data
-    } else {
-      return (
-        <main>
-          Projects
-          {repoData != null ? (
-          <div>
-            <ul>
-              {repoData?.map((repo: GitHubRepoType) => (
-                  <li className="my-2 md:my-4" key={repo.id}>
-                    <ProjectCard htmlURL={repo.html_url} repoDescription={repo.description} repoName={repo.name}/>
-                  </li>
-                ))}
-            </ul>
-          </div>) : (<p>Error</p>)
-          }
-        </main>
-      );
-    }
-  }
+  
+  return (
+    <main>
+      Projects
+      {repoData != null ? (
+      <div>
+        <ul>
+          {repoData?.map((repo: GitHubRepoType) => (
+              <li className="my-2 md:my-4" key={repo.id}>
+                <ProjectCard htmlURL={repo.html_url} repoDescription={repo.description} repoName={repo.name}/>
+              </li>
+            ))}
+        </ul>
+      </div>) : (<p>Error</p>)
+      }
+    </main>
+  );
 }
