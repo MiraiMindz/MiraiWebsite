b72bc6bdddbf60388b614cd216545dbe5e8cc387
MiraiMindz <78761663+MiraiMindz@users.noreply.github.com>
2023-06-23 11:08:04 -0300

    made some changes

---
+++ src/app/lib/APIs/GitHub/Projects.ts
@@ -0,0 +1,43 @@
+import axios from 'axios';
+export async function fetchGithubPublicRepos() {
+  try {
+    const token = process.env.GITHUB_API_KEY;
+    const response = await axios.get('https://api.github.com/user/repos', {
+      headers: {
+          Authorization: `Bearer ${token}`,
+      },
+    });
+
+    const repositories = await Promise.all(
+    response.data.map(async (repo: any) => {
+      const readmeResponse = await axios.get(
+          `https://api.github.com/repos/${repo.full_name}/readme`, {
+            headers: {
+            Authorization: `Bearer ${token}`,
+            Accept: 'application/vnd.github.VERSION.raw',
+            },
+          }
+        );
+
+        return {
+          id: repo.id,
+          name: repo.name,
+          description: repo.description,
+          html_url: repo.html_url,
+          readme: readmeResponse.data,
+        };
+      })
+    );
+
+    return {
+      props: {
+        repositories,
+      },
+    };
+  } catch (error) {
+    console.error('Error fetching repositories:', error);
+    return {
+      repositories: []
+    };
+  }
+}
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,6 +1,12 @@
 "use client";
 
-export default function Projects() {
+import { fetchGithubPublicRepos } from "../lib/APIs/GitHub/Projects";
+
+
+
+export default async function Projects() {
+  const GitRepos = await fetchGithubPublicRepos();
+  console.log(GitRepos);
   return (
     <main>
       Projects
