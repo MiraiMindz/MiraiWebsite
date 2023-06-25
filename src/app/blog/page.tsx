4f6edb1d6f73156eeab2b9f7f9110cab4f2f7915
Mirai <78761663+MiraiMindz@users.noreply.github.com>
2023-06-25 00:20:38 -0300

    made some changes

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -9,29 +9,33 @@ export default async function Page() {
 
   return (
     <main className="min-h-screen min-w-full flex-grow flex justify-start items-center flex-col">
-      <h1 className="text-2xl font-black text-center font-ibmPlexMono">Blog</h1>
-      <article className="max-w-full grid gap-x-4 grid-cols-3">
-        <div className="w-full max-h-fit after:content-[' ']"></div>
+      <div className="grid grid-cols-1 gap-y-8 gap-x-0 md:grid-cols-article md:gap-y-0 md:gap-x-8 overflow-auto max-w-full">
+        <h1 className="text-2xl font-black text-center font-ibmPlexMono">Blog</h1>
+        <aside className={"hidden md:block h-fit max-h-[85vh] overflow-y-auto"}>
+          <div className="max-h-fit after:content-[' ']"></div>
+        </aside>
         <section className="max-w-full">
-          <article className="max-w-full">
-            <ul className="w-full">
-              {posts?.map(post => (
-                <li className="my-2 md:my-4" key={post?.slug}>
-                  <PostCard
-                    title={post?.title}
-                    shortSum={post?.shortSum}
-                    href={`/blog/posts/${post?.slug}`}
-                    readTime={post?.readTime?.text}
-                    date={post?.publishDate}
-                  />
-                  <hr className="rounded-full h-0.5 w-[99%] bg-neutral-950 dark:bg-neutral-50 opacity-50"/>
-                </li>
-             ))}
-            </ul>
-          </article>
-        </section>
-        <div className="max-h-fit w-full after:content-[' ']"></div>
-      </article>
+            <article className="max-w-full">
+              <ul className="w-full">
+                {posts?.map(post => (
+                  <li className="my-2 md:my-4" key={post?.slug}>
+                    <PostCard
+                      title={post?.title}
+                      shortSum={post?.shortSum}
+                      href={`/blog/posts/${post?.slug}`}
+                      readTime={post?.readTime?.text}
+                      date={post?.publishDate}
+                    />
+                    <hr className="rounded-full h-0.5 w-[99%] bg-neutral-950 dark:bg-neutral-50 opacity-50"/>
+                  </li>
+              ))}
+              </ul>
+            </article>
+          </section>
+        <aside className={"hidden md:block h-fit max-h-[85vh] overflow-y-auto"}>
+          <div className="max-h-fit after:content-[' ']"></div>
+        </aside>
+      </div>
     </main>
   )
 }
